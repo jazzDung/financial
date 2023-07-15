838cca55a1381f28d85e74731afb9f1123c0f8a2
perceptiveperceval <34310793+perceptiveperceval@users.noreply.github.com>
2023-07-15 17:41:31 +0700

    Update create_query.py

--- dagster/financial/create_query.py
+++ dagster/financial/create_query.py
@@ -26,6 +26,7 @@ PROJECT_PATH = "/home/jazzdung/projects/financial/dbt/"
 MANIFEST_PATH = PROJECT_PATH + "target/manifest.json"
 
 
+
 MATERIALIZATION_MAPPING = {
     1:'table',
     2:'view',
@@ -194,12 +195,12 @@ def get_records():
         # """
 
         cursor.execute(postgreSQL_select_Query)
-        query_columns=['name',
+        query_columns=['query_string',
                         'materialization',
                         'user_id',
                         'description',
                         'insert_time',
-                        'query_string',
+                        'name',
                         'success',
                         'checked']
 
@@ -246,149 +247,152 @@ def update_records(df):
 def get_emails(superset, user_ids):
     res = superset.request("POST", "/security/get_email",json={"users_ids":user_ids})
     return res["emails"]
-def main():
 
-    logger = logging.getLogger(__name__)
-    # Get table names with and without schemas
-    dbt_tables_names=list(dbt_tables.keys())
+logger = logging.getLogger(__name__)
+# Get table names with and without schemas
+dbt_tables_names=list(dbt_tables.keys())
 
-    mapped = map (lambda x: x.startswith(SCHEMA_NAMES_WITH_DOT), dbt_tables_names)
-    mask=list(mapped)
+mapped = map (lambda x: x.startswith(SCHEMA_NAMES_WITH_DOT), dbt_tables_names)
+mask=list(mapped)
 
-    dbt_tables_reporting=list(compress(dbt_tables_names, mask))
+dbt_tables_reporting=list(compress(dbt_tables_names, mask))
 
-    dbt_tables_with_schemas = [table.removeprefix(schema) for table in dbt_tables_reporting for schema in SCHEMA_NAMES_WITH_DOT]
-    status = [] # Status of preliminary checking
-    
-    df = get_records()
+dbt_tables_with_schemas = [table.removeprefix(schema) for table in dbt_tables_reporting for schema in SCHEMA_NAMES_WITH_DOT]
+status = [] # Status of preliminary checking
 
+df = get_records()
+
+for i in df.index:
+    # Check name
+    print(df.loc[i]['name'])
+    name_validation = is_valid_table_name(df.loc[i]['name'])
+    print(name_validation)
+    if not name_validation:
+        status.append("Invalid name")
+        df.loc[i,'success'] = False
+        # df.loc[i,'checked'] = True
+        continue
+    # Check syntax
+    query_string = df.loc[i]['query_string']
+    query_string = query_string + ";" if query_string[-1] != ";" else query_string
+    validation = check_string(query_string)
+    if not validation[0]:
+        df.loc[i,'success'] = False
+        # df.loc[i,'checked'] = True
+        status.append("Invalid query: {error}".format(error=validation[1]))
+        continue
+    # Check multi-query
+    if len(sqlparse.split(query_string))>1:
+        df.loc[i,'success'] = False
+        # df.loc[i,'checked'] = True
+        status.append("Multiple statement")
+        continue
+    if sqlparse.parse(query_string)[0].get_type() != "SELECT":
+        df.loc[i,'success'] = False
+        # df.loc[i,'checked'] = True
+        status.append("Query is not 'SELECT'")
+        continue
+    model_path = PROJECT_PATH + "models/user/{name}.sql".format(name=df.loc[i,'name'])
+    with open(model_path,"w+") as f:
+        f.write(create_dbt_model(df.loc[i], dbt_tables_with_schemas))
+        f.close()
+    status.append("Success")
+print(status)
+# Get Emails from API
+# superset = SupersetDBTConnector(logger=logger,refresh_token=True)
+# users = set(df["user_id"].to_list())
+# emails = get_emails(superset, users)
+# email_dict = dict(zip(df.user_id.to_list(), emails))
+email_dict = {1:"catvu113@gmail"}
+
+import smtplib, ssl
+
+port = 465  # For SSL
+smtp_server = "smtp.gmail.com"
+sender_email = "catvu113@gmail.com"  
+password = 'xhtzakhmnsbufufy'
+
+context = ssl.create_default_context()
+with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
     for i in df.index:
-        # Check name
-        name_validation = is_valid_table_name(df.loc[i]['name'])
-        if not name_validation:
-            status.append("Invalid name")
-            df.loc[i,'success'] = False
-            # df.loc[i,'checked'] = True
-            continue
-        # Check syntax
-        query_string = df.loc[i]['query_string']
-        query_string = query_string + ";" if query_string[-1] != ";" else query_string
-        validation = check_string(query_string)
-        if not validation[0]:
-            df.loc[i,'success'] = False
-            # df.loc[i,'checked'] = True
-            status.append("Invalid query: {error}".format(error=validation[1]))
-            continue
-        # Check multi-query
-        if len(sqlparse.split(query_string))>1:
-            df.loc[i,'success'] = False
-            # df.loc[i,'checked'] = True
-            status.append("Multiple statement")
-            continue
-        if sqlparse.parse(query_string)[0].get_type() != "SELECT":
-            df.loc[i,'success'] = False
-            # df.loc[i,'checked'] = True
-            status.append("Query is not 'SELECT'")
-            continue
-        model_path = PROJECT_PATH + "models/user/{name}.sql".format(name=df.loc[i,'name'])
-        with open(model_path,"w") as f:
-            f.write(create_dbt_model(df.loc[i], dbt_tables_with_schemas))
-            f.close()
-        status.append("Success")
-    print(status)
-    # Get Emails from API
-    # superset = SupersetDBTConnector(logger=logger,refresh_token=True)
-    # users = set(df["user_id"].to_list())
-    # emails = get_emails(superset, users)
-    # email_dict = dict(zip(df.user_id.to_list(), emails))
-    email_dict = {1:"catvu113@gmail"}
+        # Check Success
+        if  df.loc[i,"success"] == False:
 
-    import smtplib, ssl
+            message = """\
+    Subject: Superset Model Creation
 
-    port = 465  # For SSL
-    smtp_server = "smtp.gmail.com"
-    sender_email = "catvu113@gmail.com"  
-    password = 'xhtzakhmnsbufufy'
-
-    context = ssl.create_default_context()
-    with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
-        for i in df.index:
-            # Check Success
-            if  df.loc[i,"success"] == False:
-
-                message = """\
-        Subject: Superset Model Creation
-
-        Your Model was unsuccessfully created.
-        
-        Reason:
-        {reason}
-
-        SQL:
-        {sql}
-        """.format(reason=status[i], sql=df.loc[i,'query_string'])
-
-                df.loc[i,'checked'] = True
-                server.login(sender_email, password)
-                server.sendmail(sender_email, email_dict[df.loc[i,'user_id']], message)
-
+    Your Model was unsuccessfully created.
     
+    Reason:
+    {reason}
+
+    SQL:
+    {sql}
+    """.format(reason=status[i], sql=df.loc[i,'query_string'])
+
+            df.loc[i,'checked'] = True
+            server.login(sender_email, password)
+            server.sendmail(sender_email, email_dict[df.loc[i,'user_id']], message)
 
 
-    # initialize
-    dbt = dbtRunner()
 
-    # create CLI args as a list of strings
-    cli_args = ["run", 
-                "--select", 
-                # "tag:{exec_time}".format(exec_time=EXEC_TIME)
-                "tag:user_created"
-                ]
 
-    # run the command
-    res: dbtRunnerResult = dbt.invoke(cli_args)
+# initialize
+dbt = dbtRunner()
 
-    # inspect the results
+# create CLI args as a list of strings
+cli_args = ["run", 
+            "--project-dir",
+            PROJECT_PATH,
+            "--select", 
+            # "tag:{exec_time}".format(exec_time=EXEC_TIME)
+            "tag:user_created"
+            ]
+
+# run the command
+res: dbtRunnerResult = dbt.invoke(cli_args)
+
+# inspect the results
+for r in res.result:
+    print(f"{r.node.name}: {r.status}")
+# Map df index to result
+dbt_res_df_map={}
+
+for i in df.index:
     for r in res.result:
-        print(f"{r.node.name}: {r.status}")
-    # Map df index to result
-    dbt_res_df_map={}
+        if r.node.name == df.loc[i,'name']: dbt_res_df_map[i]=r
+        break
+context = ssl.create_default_context()
 
+
+with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
     for i in df.index:
-        for r in res.result:
-            if r.node.name == df.loc[i,'name']: dbt_res_df_map[i]=r
-            break
-    context = ssl.create_default_context()
+        # Check Success
+        if df.loc[i,"success"] is None or df.loc[i,"success"] is True:
+            if dbt_res_df_map[i].status=="success":
+                message = """\
+    Subject: Superset Model Creation
 
+    Your Model was unsuccessfully created.
 
-    with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
-        for i in df.index:
-            # Check Success
-            if df.loc[i,"success"] is None or df.loc[i,"success"] is True:
-                if dbt_res_df_map[i].status=="success":
-                    message = """\
-        Subject: Superset Model Creation
+    SQL:{sql}
+    """.format(sql=df.loc[i,'query_string'])
+            else:
+                message = """\
+    Subject: Superset Model Creation
 
-        Your Model was unsuccessfully created.
+    Your Model was successfully created during dbt's run, please contact the administrator.
+    
+    Reason:
+    {reason}
 
-        SQL:{sql}
-        """.format(sql=df.loc[i,'query_string'])
-                else:
-                    message = """\
-        Subject: Superset Model Creation
+    SQL:
+    {sql}
+    """.format(reason=dbt_res_df_map[i].message, sql=df.loc[i,'query_string'])
 
-        Your Model was successfully created during dbt's run, please contact the administrator.
-        
-        Reason:
-        {reason}
+            df.loc[i,'checked'] = True
+            server.login(sender_email, password)
+            server.sendmail(sender_email, email_dict[df.loc[i,'user_id']], message)
 
-        SQL:
-        {sql}
-        """.format(reason=dbt_res_df_map[i].message, sql=df.loc[i,'query_string'])
-
-                df.loc[i,'checked'] = True
-                server.login(sender_email, password)
-                server.sendmail(sender_email, email_dict[df.loc[i,'user_id']], message)
-
-    entries_to_update = str(tuple(zip(df.name,df.user_id,df.checked,df.success))).replace("None","Null")[1:-1]
-    update_records(entries_to_update)
+entries_to_update = str(tuple(zip(df.name,df.user_id,df.checked,df.success))).replace("None","Null")[1:-1]
+update_records(entries_to_update)
