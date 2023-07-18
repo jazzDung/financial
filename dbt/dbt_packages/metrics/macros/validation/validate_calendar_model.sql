a681caf7d0fc521f0573b2571977b5925e162584
dung.pham_de <dung.pham@dnse.com.vn>
2023-07-18 11:21:29 +0700

    new dbt_expectations

--- dbt/dbt_packages/metrics/macros/validation/validate_calendar_model.sql
+++
@@ -1,5 +0,0 @@
-{% macro validate_calendar_model() %}
-
-    {% set calendar_relation = metrics.get_model_relation(var('dbt_metrics_calendar_model', "dbt_metrics_default_calendar"))%}
-
-{% endmacro %}
