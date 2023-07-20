ede7b20271a6250e3c6e88c34344dbdaf787c37e
jazzDung <dungpham.020901@gmail.com>
2023-07-20 13:34:47 +0700

    improve sma

--- dbt/target/run/dbt_financial/models/intermediate/int_bollinger.sql
+++ dbt/target/run/dbt_financial/models/intermediate/int_bollinger.sql
@@ -66,18 +66,62 @@ standard_deviation as (
             partition by ticker
             ORDER BY
                 ticker,
+                trading_date asc ROWS BETWEEN 5 PRECEDING
+                AND CURRENT ROW
+        ) AS std_dev_5,
+        stddev(close) OVER(
+            partition by ticker
+            ORDER BY
+                ticker,
+                trading_date asc ROWS BETWEEN 10 PRECEDING
+                AND CURRENT ROW
+        ) AS std_dev_10,
+        stddev(close) OVER(
+            partition by ticker
+            ORDER BY
+                ticker,
                 trading_date asc ROWS BETWEEN 20 PRECEDING
                 AND CURRENT ROW
-        ) AS std_dev
+        ) AS std_dev_20,
+        stddev(close) OVER(
+            partition by ticker
+            ORDER BY
+                ticker,
+                trading_date asc ROWS BETWEEN 50 PRECEDING
+                AND CURRENT ROW
+        ) AS std_dev_50,
+        stddev(close) OVER(
+            partition by ticker
+            ORDER BY
+                ticker,
+                trading_date asc ROWS BETWEEN 100 PRECEDING
+                AND CURRENT ROW
+        ) AS std_dev_100,
+        stddev(close) OVER(
+            partition by ticker
+            ORDER BY
+                ticker,
+                trading_date asc ROWS BETWEEN 200 PRECEDING
+                AND CURRENT ROW
+        ) AS std_dev_200
     from
         moving_average
 ),
 bollinger as (
     select
         *,
-        sma_5 as mid_band,
-        sma_5 + (2 * std_dev) as upper_band,
-        sma_5 - (2 * std_dev) as lower_band
+        sma_5 + (2 * std_dev_5) as upper_band_5,
+        sma_5 - (2 * std_dev_5) as lower_band_5,
+        sma_10 + (2 * std_dev_10) as upper_band_10,
+        sma_10 - (2 * std_dev_10) as lower_band_10,
+        sma_20 + (2 * std_dev_20) as upper_band_20,
+        sma_20 - (2 * std_dev_20) as lower_band_20,
+        sma_50 + (2 * std_dev_50) as upper_band_50,
+        sma_50 - (2 * std_dev_50) as lower_band_50,
+        sma_100 + (2 * std_dev_100) as upper_band_100,
+        sma_100 - (2 * std_dev_100) as lower_band_100,
+        sma_200 + (2 * std_dev_200) as upper_band_200,
+        sma_200 - (2 * std_dev_200) as lower_band_200
     from
         standard_deviation
 )
@@ -85,14 +129,59 @@ select
     ticker,
     close,
     case
-        when lower_band is null then 0
-        else round(CAST(lower_band as numeric), 2)
-    end as lower_band,
-    round(CAST(mid_band as numeric), 2) as mid_band,
+        when lower_band_5 is null then 0
+        else round(CAST(lower_band_5 as numeric), 2)
+    end as lower_band_5,
+    round(CAST(sma_5 as numeric), 2) as sma_5,
     case
-        when upper_band is null then 0
-        else round(CAST(upper_band as numeric), 2)
-    end as upper_band,
+        when upper_band_5 is null then 0
+        else round(CAST(upper_band_5 as numeric), 2)
+    end as upper_band_5,
+    case
+        when lower_band_10 is null then 0
+        else round(CAST(lower_band_10 as numeric), 2)
+    end as lower_band_10,
+    round(CAST(sma_10 as numeric), 2) as sma_10,
+    case
+        when upper_band_10 is null then 0
+        else round(CAST(upper_band_10 as numeric), 2)
+    end as upper_band_10,
+    case
+        when lower_band_20 is null then 0
+        else round(CAST(lower_band_20 as numeric), 2)
+    end as lower_band_20,
+    round(CAST(sma_20 as numeric), 2) as sma_20,
+    case
+        when upper_band_20 is null then 0
+        else round(CAST(upper_band_20 as numeric), 2)
+    end as upper_band_20,
+    case
+        when lower_band_50 is null then 0
+        else round(CAST(lower_band_50 as numeric), 2)
+    end as lower_band_50,
+    round(CAST(sma_50 as numeric), 2) as sma_50,
+    case
+        when upper_band_50 is null then 0
+        else round(CAST(upper_band_50 as numeric), 2)
+    end as upper_band_50,
+    case
+        when lower_band_100 is null then 0
+        else round(CAST(lower_band_100 as numeric), 2)
+    end as lower_band_100,
+    round(CAST(sma_100 as numeric), 2) as sma_100,
+    case
+        when upper_band_100 is null then 0
+        else round(CAST(upper_band_100 as numeric), 2)
+    end as upper_band_100,
+    case
+        when lower_band_200 is null then 0
+        else round(CAST(lower_band_200 as numeric), 2)
+    end as lower_band_200,
+    round(CAST(sma_200 as numeric), 2) as sma_200,
+    case
+        when upper_band_200 is null then 0
+        else round(CAST(upper_band_200 as numeric), 2)
+    end as upper_band_200,
     trading_date
 from
     bollinger
