753916c00fce8a494a6f76e9b4f5ba60c7ca4356
jazzDung <dungpham.020901@gmail.com>
2023-07-20 13:51:28 +0700

    something something

---
+++ dagster/financial/assets/dbt_assets.py
@@ -0,0 +1,19 @@
+import os
+from dagster import file_relative_path, with_resources
+from financial.resources import DBT_PROFILE_PATH, DBT_PROJECT_DIR, DBT_TARGET
+from dagster_dbt import DbtCliClientResource, load_assets_from_dbt_project
+
+dbt_assets = with_resources(
+    load_assets_from_dbt_project(
+        project_dir=DBT_PROJECT_DIR,
+        profiles_dir=DBT_PROFILE_PATH,
+        target_dir=DBT_TARGET,
+        display_raw_sql=True,
+    ),
+    {
+        "dbt": DbtCliClientResource(
+            project_dir=DBT_PROJECT_DIR,
+            profiles_dir=DBT_PROFILE_PATH,
+        ),
+    },
+)
