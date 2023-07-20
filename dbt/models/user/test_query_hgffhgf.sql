d566ee8d9e1b9f4f8645be94a4e04127bd863f76
jazzDung <dungpham.020901@gmail.com>
2023-07-20 17:39:11 +0700

    lmao

--- dbt/models/user/test_query_hgffhgf.sql
+++ dbt/models/user/test_query_hgffhgf.sql
@@ -3,12 +3,11 @@
     materialized='table',
     name='test_query_hgffhgf',
     description='test description',
-    tags = ['1','user_created','20/07/2023_16:58:56'],
+    tags = ['1','user_created','20/07/2023_17:30:30'],
     schema = 'financial_user'
 ) }}
     -- depends_on: {{ref('dim_price_history')}}
-        ),
-        
+WITH
 original_query as (
     SELECT * from marts.dim_price_history
 )
